4365e2f2b905238cc1441995383e2be05c0c2310
Abmaellf <abmael.ninha@gmail.com>
2024-01-05 23:38:57 -0400

    feat(1-2-1-CSS Modules)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
 
 export function App() {
 
   return (
     <>
+      <Header/>
+
       <Post 
         author="Abmael"
         content="Conteudo Conteudo ConteudoConteudo Conteudo Conteudo"
       />
+      
       <Post
         author="Rafael"
         content="Conteudo Conteudo Conteudo Conteudo Conteudo"
