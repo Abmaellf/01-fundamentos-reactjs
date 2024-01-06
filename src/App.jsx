a7eeefb92afc3f4469fcac420c5f0935d1692379
Abmaellf <abmael.ninha@gmail.com>
2024-01-05 22:47:18 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Post } from './Post';
+
+export function App() {
+
+  return (
+    <>
+      <Post 
+        author="Abmael"
+        content="Conteudo Conteudo ConteudoConteudo Conteudo Conteudo"
+      />
+      <Post
+        author="Rafael"
+        content="Conteudo Conteudo Conteudo Conteudo Conteudo"
+      />
+     
+      
+    </>
+  )
+}
+
---
+++ src/Post.jsx
@@ -0,0 +1,9 @@
+export function Post (props) {
+    return (
+        <>
+           <strong> <p> {props.author} </p> </strong>
+            <p> {props.content} </p>
+        </>
+    )
+    
+} 
