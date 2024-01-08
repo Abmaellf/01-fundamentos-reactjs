35a28af93ef0f4ad0083fdc4c314e7c821f5e7c3
Abmaellf <abmael.ninha@gmail.com>
2024-01-07 23:25:30 -0400

    feat(1-2-3-Componente. Header	)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
     import style from './Header.module.css';
+    import igniteLogo from '../assets/Ignite-logo.svg'
+
+    console.log(igniteLogo);
 
     export function Header () {
         return(
             <header className={style.header}>
-                <strong>Ignite Feed</strong>
+               <img src={igniteLogo} alt='Ignate logo' />
             </header>
         )
     }
