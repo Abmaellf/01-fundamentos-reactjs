562e6a112594954c77f577a5e40dbcf0463f7399
Abmaellf <abmael.ninha@gmail.com>
2024-01-11 23:04:00 -0400

    1-2-4-Componente. Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
+import styles from './App.module.css';
+
 import './global.css'
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
 
@@ -8,18 +11,26 @@ export function App() {
     <>
       <Header/>
 
-      <Post 
-        author="Abmael"
-        content="Conteudo Conteudo ConteudoConteudo Conteudo Conteudo"
-      />
-      
-      <Post
-        author="Rafael"
-        content="Conteudo Conteudo Conteudo Conteudo Conteudo"
-      />
+      <div className={styles.wrapper}>
+
+        <Sidebar/>
+
+        <main>
+              <Post 
+                author="Abmael"
+                content="Conteudo Conteudo ConteudoConteudo Conteudo Conteudo"
+              />
+            
+              <Post
+                author="Rafael"
+                content="Conteudo Conteudo Conteudo Conteudo Conteudo"
+              />  
+
+        </main>
+      </div>
+
      
-      
-    </>
+   </>
   )
 }
 
